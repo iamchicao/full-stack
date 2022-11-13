f50c6494a0819f90bb00edac5aefcb2a28be880a
Francisco Junior <jr-francis2011@hotmail.com>
2022-11-13 18:31:42 -0300

    feat: Add hook to managment state component

--- part1/lessons/count-click/src/App.js
+++ part1/lessons/count-click/src/App.js
@@ -1,6 +1,18 @@
-const App = (props) => {
-  const { counter } = props;
-  return <div>{counter}</div>;
+import { useState } from 'react';
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
+
+  console.log('rendering...', counter);
+
+  return (
+    <div>{counter}</div>
+  )
 };
 
 export default App;
