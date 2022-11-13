98918b98b1bea0907277c9a8a786116937f35b24
Francisco Junior <jr-francis2011@hotmail.com>
2022-11-12 23:48:42 -0300

    ...part2 props

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,19 +1,23 @@
 const Hello = (props) => {
   return (
     <div>
-      <p>{props.name} and my position on div is {props.position}</p>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
     </div>
   )
 }
 
 
 const App = () => {
+  const name = 'Francisco';
+  const age = 24
+
   return (
     <div>
       <h1>Grettings</h1>
-      <Hello name="George" position={1} />
-      <Hello name="Lucas" position={2} />
-      <Hello name="Francisco" position={3} />
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
     </div>
   );
 };
