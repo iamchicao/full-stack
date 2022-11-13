3d57bd3c1388dff821ad0f7505f043d43934f1c1
Francisco Junior <jr-francis2011@hotmail.com>
2022-11-13 18:42:40 -0300

    feat: add events manipulation to clicked button

--- part1/lessons/count-click/src/App.js
+++ part1/lessons/count-click/src/App.js
@@ -1,18 +1,19 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const App = () => {
   const [counter, setCounter] = useState(0);
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
-
-  console.log('rendering...', counter);
-
   return (
-    <div>{counter}</div>
-  )
+    <div>
+      <div>{counter}</div>
+      <button onClick={() => setCounter(counter + 1)}>
+        plus
+      </button>
+      <button onClick={() => setCounter(0)}>
+        zero
+      </button>
+    </div>
+  );
 };
 
 export default App;
