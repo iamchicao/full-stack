98a96745b41b655050beb9c9022fc14cc2d1847f
Francisco Junior <jr-francis2011@hotmail.com>
2022-11-13 00:24:04 -0300

    refactor: change folder name

---
+++ exampleapp/src/App.js
@@ -0,0 +1,33 @@
+const Hello = (props) => {
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  );
+};
+
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href="https://github.com/mluukkai">mluukkai</a>
+    </div>
+  );
+};
+
+const App = () => {
+  const name = 'Francisco';
+  const age = 24;
+
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name='Maya' age={26 + 10} />
+      <Hello name={name} age={age} />
+      <Footer />
+    </>
+  )
+}
+
+export default App;
