e6da2a8b82c3d7cd12aef15209d9313c2e7b1d59
Francisco Junior <jr-francis2011@hotmail.com>
2022-11-13 19:53:40 -0300

    refactor: change the components

--- part1/lessons/count-click/src/App.js
+++ part1/lessons/count-click/src/App.js
@@ -1,12 +1,8 @@
 import { useState } from "react";
 
-const Display = (props) => {
-  return <div>{props.counter}</div>;
-};
+const Display = ({counter}) => <div>{counter}</div>;
 
-const Button = (props) => {
-  return <button onClick={props.onClick}>{props.text}</button>;
-};
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
   const [counter, setCounter] = useState(0);
