9deafa2b80367d9f1171dccf669e2e1a46d7c333
Francisco Junior <jr-francis2011@hotmail.com>
2022-11-15 18:40:32 -0300

    feat: Add Button

--- part1/lessons/left-right/src/App.js
+++ part1/lessons/left-right/src/App.js
@@ -1,29 +1,41 @@
-import { useState } from "react"
+import { useState } from "react";
+
+const History = (props) => {
+  if (props.allClicks.lenght === 0) {
+    return <div>the app is used by pressing the butons</div>;
+  }
+  return <div>button press history: {props.allClicks.join("")}</div>;
+};
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAllClicks] = useState([])
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAllClicks] = useState([]);
 
   const handleLeftClick = () => {
-    setAllClicks(allClicks.concat('L'))
-    setLeft(left + 1)
-  }
+    setAllClicks(allClicks.concat("L"));
+    setLeft(left + 1);
+  };
 
   const handleRightClick = () => {
-    setAllClicks(allClicks.concat('R'))
-    setRight(right + 1)
-  }
+    setAllClicks(allClicks.concat("R"));
+    setRight(right + 1);
+  };
 
   return (
     <div>
       {left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
+      <Button handleClick={handleLeftClick} text='left'/>
+      <Button handleClick={handleRightClick} text='right'/>
       {right}
-      <p>{allClicks.join(' ')}</p>
+      <p>{allClicks.join(" ")}</p>
+      <History allClicks={allClicks} />
     </div>
-  )
-}
+  );
+};
 
 export default App;
