b635fe3cf5ead0e6e1ae2145726046099e65a2fc
Francisco Junior <jr-francis2011@hotmail.com>
2022-12-10 18:53:52 -0300

    refactor: Add StaticLine component

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,15 +2,38 @@ import { useState } from "react";
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
+const StaticLine = ({ text, value }) => (
+  <p>
+    {text} {value}
+  </p>
+);
+
+const Statics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  const average = (good - bad) / total;
+  const positive = `${(good / total) * 100} %`;
+
+  return total > 0 ? (
+    <>
+      <h2>statics</h2>
+      <StaticLine text="good" value={good} />
+      <StaticLine text="neutral" value={neutral} />
+      <StaticLine text="bad" value={bad} />
+      <StaticLine text="total" value={total} />
+      <StaticLine text="average" value={average} />
+      <StaticLine text="positive" value={positive} />
+    </>
+  ) : (
+    <p>No feedback given</p>
+  );
+};
+
 export const App = () => {
   const [good, setGod] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
   const increment = (state, setState) => () => setState(state + 1);
-  const total = good + neutral + bad;
-  const average = ((good - bad) / total);
-  const positive = (good / total) * 100
 
   return (
     <div>
@@ -18,13 +41,7 @@ export const App = () => {
       <Button onClick={increment(good, setGod)} text="good" />
       <Button onClick={increment(neutral, setNeutral)} text="neutral" />
       <Button onClick={increment(bad, setBad)} text="bad" />
-      <h2>statics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>average {average}</p>
-      <p>positive {positive} %</p>
+      <Statics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
