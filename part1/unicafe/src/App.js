49c4f01969d539a9aca1d0513e4333e150a90eec
Francisco Junior <jr-francis2011@hotmail.com>
2022-12-10 18:36:36 -0300

    feat: Add total, average and positive rates

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -8,6 +8,9 @@ export const App = () => {
   const [bad, setBad] = useState(0);
 
   const increment = (state, setState) => () => setState(state + 1);
+  const total = good + neutral + bad;
+  const average = ((good - bad) / total);
+  const positive = (good / total) * 100
 
   return (
     <div>
@@ -15,12 +18,13 @@ export const App = () => {
       <Button onClick={increment(good, setGod)} text="good" />
       <Button onClick={increment(neutral, setNeutral)} text="neutral" />
       <Button onClick={increment(bad, setBad)} text="bad" />
-
-      
       <h2>statics</h2>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </div>
   );
 };
