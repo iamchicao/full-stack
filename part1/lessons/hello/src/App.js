e1bb21fd0ebbb45f9b3778ad96458f566f442a5b
Francisco Junior <jr-francis2011@hotmail.com>
2022-12-10 17:44:48 -0300

    feat: add function to return function

--- part1/lessons/hello/src/App.js
+++ part1/lessons/hello/src/App.js
@@ -1,24 +1,21 @@
 import { useState } from "react";
 
 const App = () => {
-  const [value, setValue] = useState(10)
+  const [value, setValue] = useState(10);
 
-  const handleChange = () => {
-    console.log('cliked the button')
-    setValue(0)
-  }
-
-  const hello = () => {
-    const handler = () => console.log('Hello world')
-    return handler
-  }
+  const setToValue = (newValue) => () => {
+    console.log("value now", newValue); // print new value to console
+    setValue(newValue);
+  };
 
   return (
     <div>
       {value}
-      <button onClick={hello()}>button</button>
+      <button onClick={() => setToValue(1000)}>thousand</button>
+      <button onClick={() => setToValue(0)}>reset</button>
+      <button onClick={() => setToValue(value + 1)}>increment</button>
     </div>
-  )
-}
+  );
+};
 
 export default App;
