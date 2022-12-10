2ac14923f74fcee5c60f9153530738b8e6cf114a
Francisco Junior <jr-francis2011@hotmail.com>
2022-12-10 17:54:23 -0300

    feat: Do not define Comopnents within Componentes

--- part1/lessons/hello/src/App.js
+++ part1/lessons/hello/src/App.js
@@ -1,20 +1,23 @@
 import React, { useState } from "react";
-// import { ReactDOM } from "react";
 
+// This is the right place to define a component
 const Button = (props) => (
   <button onClick={props.handleClick}>{props.text}</button>
 );
+
+const Display = (props) => <div>{props.value}</div>;
 
 const App = () => {
   const [value, setValue] = useState(10);
 
   const setToValue = (newValue) => {
+    console.log("value now", newValue);
     setValue(newValue);
   };
 
   return (
     <div>
-      {value}
+      <Display value={value} />
       <Button handleClick={() => setToValue(1000)} text="thousand" />
       <Button handleClick={() => setToValue(0)} text="reset" />
       <Button handleClick={() => setToValue(value + 1)} text="increment" />
